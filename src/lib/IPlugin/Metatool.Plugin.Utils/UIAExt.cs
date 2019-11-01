ce87d39962a4177be9ae3524460026eef3eba1c3
metasong <metaseed@gmail.com>
2019-11-01 17:14:41 -0500

    ok

---
+++ src/lib/IPlugin/Metatool.Plugin.Utils/UIAExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Automation;
+using Microsoft.Extensions.Logging;
+
+namespace Metatool.Plugin
+{
+    public static class UIAExt
+    {
+        private static ILogger log = Services.Get<ILogger>();
+        public static void Select(this AutomationElement ele)
+        {
+            try
+            {
+                var itemPat = ele.GetCurrentPattern(SelectionItemPattern.Pattern) as SelectionItemPattern;
+                itemPat?.Select();
+            }
+            catch (InvalidOperationException e)
+            {
+                log.LogWarning(e.Message);
+            }
+        }
+    }
+}
--- src/tools_todo/Metatool.MetaKeyboard/FileExplorer.cs
+++ src/tools_todo/Metatool.MetaKeyboard/FileExplorer.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Windows.Automation;
 using System.Windows.Forms;
 using FlaUI.Core.AutomationElements;
 using FlaUI.UIA3;
@@ -10,6 +11,7 @@ using Metatool.Input;
 using Metatool.Plugin;
 using Metatool.Utils;
 using static Metatool.Input.Key;
+using AutomationElement = System.Windows.Automation.AutomationElement;
 
 namespace Metatool.MetaKeyboard
 {
@@ -27,27 +29,41 @@ namespace Metatool.MetaKeyboard
             return "CabinetWClass" == c;
         }
 
-        public IKey  FocusFileItemsView = (LWin + F).Down(e =>
+        public IKey FocusFileItemsView = (LWin + F).Down(e =>
         {
-            using (var automation = new UIA3Automation())
+            var winHandle  = Utils.Window.CurrentWindowHandle;
+            var winElement = AutomationElement.FromHandle(winHandle);
+            var listBoxEle = winElement.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.ClassNameProperty, "UIItemsView"));
+            var listBox = listBoxEle.GetCurrentPattern(SelectionPattern.Pattern) as SelectionPattern;
+            var selectedItem = listBox?.Current.GetSelection().FirstOrDefault();
+            if (selectedItem != null) selectedItem.SetFocus();
+            else
             {
-                var h = Utils.Window.CurrentWindowHandle;
-                var element = automation.FromHandle(h);
-                var listBox = element.FindFirstDescendant(cf => cf.ByClassName("UIItemsView"))?.AsListBox();
-                if (listBox?.SelectedItem != null) listBox.SelectedItem.Focus();
-                else listBox?.Items.FirstOrDefault()?.Select();
+                var firstItem = listBoxEle.FindFirst(TreeScope.Children,
+                    new PropertyCondition(AutomationElement.ClassNameProperty, "UIItem"));
+                firstItem?.Select();
             }
+
+            // using (var automation = new UIA3Automation())
+            // {
+            //     var h = Utils.Window.CurrentWindowHandle;
+            //     var element = automation.FromHandle(h);
+            //     var listBox = element.FindFirstDescendant(cf => cf.ByClassName("UIItemsView"))?.AsListBox();
+            //     if (listBox?.SelectedItem != null) listBox.SelectedItem.Focus();
+            //     else listBox?.Items.FirstOrDefault()?.Select();
+            // }
 
             e.Handled = true;
         }, IsExplorerOrDialog, "Focus &File Items View");
 
 
-        public IKey  FocusNavigationTreeView = (LWin + N).Down(e =>
+        public IKey FocusNavigationTreeView = (LWin + N).Down(e =>
         {
             using (var automation = new UIA3Automation())
             {
-                var h = Utils.Window.CurrentWindowHandle;
-                var element = automation.FromHandle(h);
+                var h        = Utils.Window.CurrentWindowHandle;
+                var element  = automation.FromHandle(h);
                 var treeView = element.FindFirstDescendant(cf => cf.ByClassName("SysTreeView32"));
                 treeView?.AsTree().SelectedTreeItem.Focus();
             }
@@ -55,23 +71,23 @@ namespace Metatool.MetaKeyboard
             e.Handled = true;
         }, IsExplorerOrDialog, "Focus &Navigation Tree View");
 
-        public IKey  CopySelectedPath = (Caps + Pipe).Down(async e =>
+        public IKey CopySelectedPath = (Caps + Pipe).Down(async e =>
         {
             var handle = Utils.Window.CurrentWindowHandle;
-            var paths = await Explorer.GetSelectedPath(handle);
-            var r = string.Join(';', paths);
+            var paths  = await Explorer.GetSelectedPath(handle);
+            var r      = string.Join(';', paths);
             System.Windows.Clipboard.SetText(r);
             e.Handled = true;
         }, IsExplorerOrDialog, "Copy Selected Files Path");
 
 
-        public IKey  NewFile = (Ctrl + Alt + N).Hit(async e =>
+        public IKey NewFile = (Ctrl + Alt + N).Hit(async e =>
         {
             const string newFileName = "NewFile";
-            var handle = Utils.Window.CurrentWindowHandle;
-            var fullPath = await Explorer.Path(handle);
-            var fileName = newFileName;
-            var i = 1;
+            var          handle      = Utils.Window.CurrentWindowHandle;
+            var          fullPath    = await Explorer.Path(handle);
+            var          fileName    = newFileName;
+            var          i           = 1;
             while (File.Exists(fullPath + "\\" + fileName))
             {
                 fileName = newFileName + i++;
@@ -80,13 +96,12 @@ namespace Metatool.MetaKeyboard
             var file = File.Create(fullPath + "\\" + fileName);
             file.Close();
             var keyboard = Services.Get<IKeyboard>();
-            Explorer.Select(handle, new[] { fileName });
+            Explorer.Select(handle, new[] {fileName});
             keyboard.Type(Keys.F2);
         }, IsExplorer, "&New File");
 
-        public IKey  ShowDesktopFolder = (LWin + D).Down(e =>
+        public IKey ShowDesktopFolder = (LWin + D).Down(e =>
             {
-                
                 Explorer.Open(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
                 e.Handled = true;
             }
