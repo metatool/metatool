70948d613c3d265ae9e3921244582a21e42d9bf3
metasong <metaseed@gmail.com>
2019-11-11 17:07:18 -0600

    fix

--- src/lib/Metatool.NugetPackage/NugetHelper.cs
+++ src/lib/Metatool.NugetPackage/NugetHelper.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Versioning;
-using Metatool.Service;
 using NuGet.Configuration;
 using NuGet.Frameworks;
 using NuGet.Packaging;
