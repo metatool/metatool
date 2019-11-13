308d0845de0e5c31e306d3be64b5be0b695212f4
metasong <metaseed@gmail.com>
2019-11-13 17:33:09 -0600

    o

--- src/tools_todo/Metatool.MetaKeyboard/FunctionalKeys.cs
+++ src/tools_todo/Metatool.MetaKeyboard/FunctionalKeys.cs
@@ -9,32 +9,28 @@ namespace Metatool.MetaKeyboard
         {
             RegisterCommands();
         }
-        public IKeyCommand  CloseMetaKeysCommand = (LWin + L).Handled().Down(null);
 
+        public IKeyCommand CloseMetaKeysCommand = (LWin + L).Handled().Down(null);
 
-        public IKeyCommand  CloseMetaKeyCommand = (LCtrl + LWin + LAlt +  C)
-            .Down(e =>
-            {
-                var notify = Services.Get<INotify>();
-                notify.ShowMessage("MetaKeyBoard Closing...");
-                Context.Exit(0);
-            }, null, "Close");
+        public IKeyCommand CloseMetaKeyCommand = (LCtrl + LWin + LAlt + C).Down(e =>
+        {
+            var notify = Services.Get<INotify>();
+            notify.ShowMessage("MetaKeyBoard Closing...");
+            Context.Exit(0);
+        }, null, "Close");
 
-        public IKeyCommand  RestartMetakeyAdmin = (LCtrl + LWin + LAlt + X).Down(e =>
+        public IKeyCommand RestartMetatoolAdmin = (LCtrl + LWin + LAlt + X).Down(e =>
         {
             var notify = Services.Get<INotify>();
             notify.ShowMessage("MetaKeyBoard Restarting...");
             Context.Restart(0, true);
         }, null, "Restart(Admin)");
 
-        public IKeyCommand  ShowTips = (Caps + Question).Down(e =>
+        public IKeyCommand ShowTips = (Caps + Question).Down(e =>
         {
             var keyboard = Services.Get<IKeyboard>();
-
             //Keyboard.Default.ShowTip();
             e.Handled = true;
         }, null, "Show Tips");
-
-      
     }
 }
