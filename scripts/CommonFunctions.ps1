7136734554fd9c5d7928f22b78f745ad7099202a
metasong <metaseed@gmail.com>
2020-05-12 21:58:17 -0500

    ok

--- scripts/CommonFunctions.ps1
+++ scripts/CommonFunctions.ps1
@@ -12,9 +12,50 @@ function Read-MessageBoxDialog([string]$Message, [string]$WindowTitle, [System.W
 }
 
 function Read-InputBoxDialog([string]$Message, [string]$WindowTitle, [string]$DefaultText)
-{[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
-	Add-Type -AssemblyName Microsoft.VisualBasic
-	return [Microsoft.VisualBasic.Interaction]::InputBox($Message, $WindowTitle, $DefaultText)
+{    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
+$objForm = New-Object System.Windows.Forms.Form
+$objForm.Text = $formTitle
+$objForm.Size = New-Object System.Drawing.Size(300,200)
+$objForm.StartPosition = "CenterScreen"
+
+$objForm.KeyPreview = $True
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") {$x=$objTextBox.Text;$objForm.Close()}})
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") {$objForm.Close()}})
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Size(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+$OKButton.Add_Click({$Script:userInput=$objTextBox.Text;$objForm.Close()})
+$objForm.Controls.Add($OKButton)
+
+$CANCELButton = New-Object System.Windows.Forms.Button
+$CANCELButton.Location = New-Object System.Drawing.Size(150,120)
+$CANCELButton.Size = New-Object System.Drawing.Size(75,23)
+$CANCELButton.Text = "CANCEL"
+$CANCELButton.Add_Click({$objForm.Close()})
+$objForm.Controls.Add($CANCELButton)
+
+$objLabel = New-Object System.Windows.Forms.Label
+$objLabel.Location = New-Object System.Drawing.Size(10,20)
+$objLabel.Size = New-Object System.Drawing.Size(280,30)
+$objLabel.Text = $textTitle
+$objForm.Controls.Add($objLabel)
+
+$objTextBox = New-Object System.Windows.Forms.TextBox
+$objTextBox.Location = New-Object System.Drawing.Size(10,50)
+$objTextBox.Size = New-Object System.Drawing.Size(260,20)
+$objForm.Controls.Add($objTextBox)
+
+$objForm.Topmost = $True
+
+$objForm.Add_Shown({$objForm.Activate()})
+
+[void] $objForm.ShowDialog()
+
+return $userInput
 }
 
 function Read-MultiLineInputBoxDialog([string]$Message, [string]$WindowTitle, [string]$DefaultText)
