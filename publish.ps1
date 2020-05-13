e19ecaf50350747639a95d96dd16584e78a1a62e
metasong <metaseed@gmail.com>
2020-05-12 22:45:13 -0500

    release work

--- publish.ps1
+++ publish.ps1
@@ -3,47 +3,54 @@ param (
     [Parameter()]
     [switch]
     [Alias("r")]
-    $rebuild
+	$rebuild,
+	[Alias("t")]
+	[switch]
+	$test
 )
 $target = $rebuild ? "rebuild": "build"
 $metatool = "M:\Workspace\metatool" 
 $tools = "$metatool\exe\release\tools"
 $publish = "$metatool\exe\publishing"
+$published = "$metatool\exe\published"
 
 Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
 Enter-VsDevShell a4cdb433
 Set-Location $metatool
 
-# if (Test-Path $publish) {
-#     Remove-Item $publish -Force -Recurse
-# }
+if (Test-Path $publish) {
+    Remove-Item $publish -Force -Recurse
+}
 
+msbuild /t:publish "$metatool\src\app\Metaseed.Metatool.csproj"  /p:DeployOnBuild=true  /p:Configuration=Release /p:PublishDir="$publish" /p:CopyOutputSymbolsToPublishDirectory=false /p:SolutionDir="$metatool\src\" /p:PublishProfile="$metatool\src\app\Properties\PublishProfiles\metatool.pubxml"
 
-# msbuild /t:publish "$metatool\src\app\Metaseed.Metatool.csproj"  /p:DeployOnBuild=true  /p:Configuration=Release /p:PublishDir="$publish" /p:CopyOutputSymbolsToPublishDirectory=false /p:SolutionDir="$metatool\src\" /p:PublishProfile="$metatool\src\app\Properties\PublishProfiles\metatool.pubxml"
+"Metatool.Tools.MetaKeyboard", "Metatool.Tools.Software" | ForEach-Object {
+    msbuild "$metatool\src\tool\$_\$_.csproj" -t:$target /p:SolutionDir=$metatool\src\ /p:Configuration=Release
+    Copy-Item "$tools\$_" -Destination "$publish\tools\$_" -Recurse -Exclude *.nupkg, *.pdb, Metatool.Service*.dll
+}
+$metaSoftware = "$metatool\exe\publish\tools\Metatool.Tools.Software"
+$metaSoftwarePublishing = "$metatool\exe\publishing\tools\Metatool.Tools.Software"
 
-# "Metatool.Tools.MetaKeyboard", "Metatool.Tools.Software" | ForEach-Object {
-#     msbuild "$metatool\src\tool\$_\$_.csproj" -t:$target /p:SolutionDir=$metatool\src\ /p:Configuration=Release
-#     Copy-Item "$tools\$_" -Destination "$publish\tools\$_" -Recurse -Exclude *.nupkg, *.pdb, Metatool.Service*.dll
-# }
-# $metaSoftware = "$metatool\exe\publish\tools\Metatool.Tools.Software"
-# $metaSoftwarePublishing = "$metatool\exe\publishing\tools\Metatool.Tools.Software"
-
-# Copy-Item "$metaSoftware\software" -Destination "$metaSoftwarePublishing\software" -Recurse
-# Copy-Item "$metaSoftware\softwareConfig" -Destination "$metaSoftwarePublishing\softwareConfig" -Recurse
+Copy-Item "$metaSoftware\software" -Destination "$metaSoftwarePublishing\software" -Recurse
+Copy-Item "$metaSoftware\softwareConfig" -Destination "$metaSoftwarePublishing\softwareConfig" -Recurse
 
 ## zip
 $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$publish\metatool.exe").ProductVersion
 
-$target = "$metatool\exe\published\Metatool.$version.zip"
-# $entries = New-Object System.Collections.Generic.List[System.Object]
-# Get-ChildItem $publish | ForEach-Object { $entries.Add($_.FullName) }
+if(!(Test-Path $published)){
+	New-Item -ItemType Directory -Force -Path $published
+}
 
-# Compress-Archive -LiteralPath $entries -CompressionLevel Optimal -DestinationPath $target -Force
+$target = "$published\Metatool.$version.zip"
+$entries = New-Object System.Collections.Generic.List[System.Object]
+Get-ChildItem $publish | ForEach-Object { $entries.Add($_.FullName) }
+
+Compress-Archive -LiteralPath $entries -CompressionLevel Optimal -DestinationPath $target -Force
 
 
 ## release
 # Set to True when testing the script to prevent actual publishing to PowerShell Gallery, and to create a Draft of the GitHub Release instead of a published Release.
-$isTestingThisScript = $true
+$isTestingThisScript = $test
 $gitHubUsername = 'metatool'
 $gitHubRepositoryName = 'metatool'
 
