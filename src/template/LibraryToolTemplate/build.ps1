f18bc6d410faee637a2892a9a10884021ee86679
metasong <metaseed@gmail.com>
2020-08-22 23:20:23 -0500

    generate  script template

--- src/template/LibraryToolTemplate/build.ps1
+++ src/template/LibraryToolTemplate/build.ps1
@@ -1,6 +1,6 @@
 
 $dir = "$PSScriptRoot\Metatool.Tools.LibTool"
-$target = "$PSScriptRoot\..\..\app\Templates\Metatool.Tools.LibTool.zip"
+$target = "$PSScriptRoot\..\..\app\Metaseed.Metatool\Templates\Metatool.Tools.LibTool.zip"
 $entries = New-Object System.Collections.Generic.List[System.Object]
 $sub = Get-ChildItem $dir -Exclude "bin", "obj" | ForEach-Object { $_.FullName }
 
