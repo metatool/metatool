e69e26a98b164d79fe480e5b7b92746a9025e0e1
metasong <metaseed@gmail.com>
2023-10-23 17:39:07 +0200

    refactor

--- src/tool/Metatool.Tools.MetaKeyboard/FileExplorer.cs
+++ src/tool/Metatool.Tools.MetaKeyboard/FileExplorer.cs
@@ -1,87 +1,88 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Windows.Automation;
-using System.Windows.Forms;
-using System.Windows.Media.Animation;
 using Metatool.Service;
 using static Metatool.Service.Key;
 
-namespace Metatool.MetaKeyboard
+namespace Metatool.MetaKeyboard;
+
+public class FileExplorer : CommandPackage
 {
-    public class FileExplorer : CommandPackage
-    {
 
-        public FileExplorer(IWindowManager windowManager, IFileExplorer fileExplorer, IConfig<Config> config, INotify notify, IKeyboard keyboard)
-        {
-            RegisterCommands();
-            var hotKeys = config.CurrentValue.FileExplorerPackage.Hotkeys;
-            hotKeys.FocusItemsView.OnEvent(e =>
-            {
-                var listBoxEle   = windowManager.CurrentWindow?.FirstDescendant(c => c.ByClassName("UIItemsView"));
-                var selectedItem = listBoxEle?.SelectedItems()?.FirstOrDefault();
-                if (selectedItem != null) selectedItem.SetFocus();
-                else listBoxEle.FirstChild(c => c.ByClassName("UIItem"))?.Select();
-                e.Handled = true;
-            }, _ => windowManager.CurrentWindow.IsExplorerOrOpenSaveDialog);
+	public FileExplorer(IWindowManager windowManager, IFileExplorer fileExplorer, IConfig<Config> config, INotify notify, IKeyboard keyboard)
+	{
+		RegisterCommands();
+		var hotKeys = config.CurrentValue.FileExplorerPackage.Hotkeys;
+		hotKeys.FocusItemsView.OnEvent(e =>
+		{
+			var listBoxEle = windowManager.CurrentWindow?.FirstDescendant(c => c.ByClassName("UIItemsView"));
+			var selectedItem = listBoxEle?.SelectedItems()?.FirstOrDefault();
+			if (selectedItem != null) selectedItem.SetFocus();
+			else listBoxEle.FirstChild(c => c.ByClassName("UIItem"))?.Select();
+			e.Handled = true;
+		}, _ => windowManager.CurrentWindow.IsExplorerOrOpenSaveDialog);
 
-            hotKeys.FocusNavigationTreeView.OnEvent(e =>
-            {
-                var winEle       = windowManager.CurrentWindow?.FirstDescendant(cf => cf.ByClassName("SysTreeView32"));
-                var selectedItem = winEle?.SelectedItems().FirstOrDefault();
-                if (selectedItem != null)
-                    selectedItem.SetFocus();
-                else
-                    winEle?.FirstDecendent(c => c.ByControlType(ControlType.TreeItem))?.Select();
+		hotKeys.FocusNavigationTreeView.OnEvent(e =>
+		{
+			var winEle = windowManager.CurrentWindow?.FirstDescendant(cf => cf.ByClassName("SysTreeView32"));
+			var selectedItem = winEle?.SelectedItems().FirstOrDefault();
+			if (selectedItem != null)
+				selectedItem.SetFocus();
+			else
+				winEle?.FirstDecendent(c => c.ByControlType(ControlType.TreeItem))?.Select();
 
-                e.Handled = true;
-            }, _ => windowManager.CurrentWindow.IsExplorerOrOpenSaveDialog);
+			e.Handled = true;
+		}, _ => windowManager.CurrentWindow.IsExplorerOrOpenSaveDialog);
 
-            hotKeys.CopySelectedPath.OnEvent(async e =>
-            {
-                e.Handled = true;
-                var handle = windowManager.CurrentWindow.Handle;
-                var paths  = await fileExplorer.GetSelectedPaths(handle);
-                var r      = string.Join(';', paths);
-                notify.ShowMessage($"Path Copied: {r}");
-                System.Windows.Clipboard.SetText(r);
-            }, _ => windowManager.CurrentWindow.IsExplorerOrOpenSaveDialog);
+		hotKeys.CopySelectedPath.OnEvent(async e =>
+		{
+			e.Handled = true;
+			var handle = windowManager.CurrentWindow.Handle;
+			var paths = await fileExplorer.GetSelectedPaths(handle);
+			var r = string.Join(';', paths);
+			notify.ShowMessage($"Path Copied: {r}");
+			System.Windows.Clipboard.SetText(r);
+		}, _ => windowManager.CurrentWindow.IsExplorerOrOpenSaveDialog);
 
-            hotKeys.NewFile.OnEvent(async e =>
-            {
-                e.Handled = true;
-                const string newFileName = "NewFile";
-                var          handle      = windowManager.CurrentWindow.Handle;
-                var          fullPath    = await fileExplorer.CurrentDirectory(handle);
-                var          fileName    = newFileName;
-                var          i           = 1;
-                while (File.Exists(fullPath + "\\" + fileName))
-                {
-                    fileName = newFileName + i++;
-                }
+		hotKeys.NewFile.OnEvent(async e =>
+		{
+			e.Handled = true;
+			const string newFileName = "NewFile";
+			var handle = windowManager.CurrentWindow.Handle;
+			var fullPath = await fileExplorer.CurrentDirectory(handle);
+			var fileName = newFileName;
+			var i = 1;
+			while (File.Exists(fullPath + "\\" + fileName))
+			{
+				fileName = newFileName + i++;
+			}
 
-                var file = File.Create(fullPath + "\\" + fileName);
-                file.Close();
-                var keyboard = Services.Get<IKeyboard>();
-                fileExplorer.Select(handle, new[] {fileName});
-                e.BeginInvoke(()=>keyboard.Type(Key.F2));
-            }, _ => windowManager.CurrentWindow.IsExplorer);
+			var file = File.Create(fullPath + "\\" + fileName);
+			file.Close();
+			var keyboard = Services.Get<IKeyboard>();
+			fileExplorer.Select(handle, new[] { fileName });
+			e.BeginInvoke(() => keyboard.Type(Key.F2));
+		}, _ => windowManager.CurrentWindow.IsExplorer);
 
-            hotKeys.ShowDesktopFolder.OnEvent(e =>
-            {
-                fileExplorer.Open(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-                e.Handled = true;
-            });
+		hotKeys.ShowDesktopFolder.OnEvent(e =>
+		{
+			fileExplorer.Open(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+			e.Handled = true;
+		});
 
-            (Ctrl + Back).OnDown(e =>
-            {
-               // fix ctrl+back is a box char in explorer
-                e.DisableVirtualKeyHandlingInThisEvent();
-                e.Handled = true;
-                keyboard.Type(Ctrl+Shift+Left, Back); // Ctrl is up now
-                keyboard.Down(Ctrl); //to trigger, if user hold ctrl and press back again
-            }, e => windowManager.CurrentWindow.IsExplorerOrOpenSaveDialog);
-        }
-    }
+		// When rename a file name: ab cd ef|
+		// to delete ef, we press ctrl + backspace, but it will become "ab cd ef'boxChar'"
+		// to fix it:
+		(Ctrl + Back).OnDown(e =>
+		{
+			// fix ctrl+back is a box char in explorer
+			e.DisableVirtualKeyHandlingInThisEvent();
+			e.Handled = true;
+			keyboard.Type(Ctrl + Shift + Left, Back); // Ctrl is up now
+			keyboard.Down(Ctrl); //to trigger, if user hold ctrl and press back again
+		},
+			e => windowManager.CurrentWindow.IsExplorerOrOpenSaveDialog,
+			"fix ctrl+backspace function to delete a word in rename");
+	}
 }
--- src/tool/Metatool.Tools.MetaKeyboard/Keyboard61.SetupWinLock.cs
+++ src/tool/Metatool.Tools.MetaKeyboard/Keyboard61.SetupWinLock.cs
@@ -6,63 +6,71 @@ using Microsoft.Win32;
 
 namespace Metatool.MetaKeyboard
 {
-    partial class Keyboard61
-    {
-        private  void SetupWinLock()
-        {
-            void EnableWinLock()
-            {
-                try
-                {
-                    Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System")?
-                        .SetValue("DisableLockWorkstation", 0, RegistryValueKind.DWord);
-                    Logger.LogInformation("WinLock Enabled");
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    Logger.LogWarning(
-                        "Could not enable WinLock(Win+L), so *+Win+L would trigger ScreenLock, press LCtrl+LWin+LAlt+X to restart with Admin rights.");
-                }
-            }
+	partial class Keyboard61
+	{
+		private void SetupWinLock()
+		{
+			void EnableWinLock()
+			{
+				try
+				{
+					Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System")?
+						.SetValue("DisableLockWorkstation", 0, RegistryValueKind.DWord);
+					Logger.LogInformation("WinLock Enabled");
+				}
+				catch (UnauthorizedAccessException)
+				{
+					Logger.LogWarning(
+						"Could not enable WinLock(Win+L), so *+Win+L would trigger ScreenLock, press LCtrl+LWin+LAlt+X to restart with Admin rights.");
+				}
+			}
 
-            void DisableWinLock()
-            {
-                try
-                {
-                    Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System")?
-                        .SetValue("DisableLockWorkstation", 1, RegistryValueKind.DWord);
-                    Logger.LogInformation("WinLock disabled!");
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    Logger.LogWarning(
-                        "Could not disable WinLock(Win+L), so *+Win+L would trigger ScreenLock, press LCtrl+LWin+LAlt+X to restart with Admin rights.");
-                }
-            }
+			void DisableWinLock()
+			{
+				try
+				{
+					Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System")?
+						.SetValue("DisableLockWorkstation", 1, RegistryValueKind.DWord);
+					Logger.LogInformation("WinLock disabled!");
+				}
+				catch (UnauthorizedAccessException)
+				{
+					Logger.LogWarning(
+						"Could not disable WinLock(Win+L), so *+Win+L would trigger ScreenLock, press LCtrl+LWin+LAlt+X to restart with Admin rights.");
+				}
+			}
 
-            DisableWinLock();
-            var winLock   = Key.Win + Key.L;
-            var winOrLKey = Key.Win | Key.L | Key.Ctrl | Key.Alt | Key.Shift;
-            var enableLock = winLock.OnDown(e =>
-            {
-                // when LWin+L pressed, enable to lock
-                EnableWinLock();
-            }, e => !e.KeyboardState.IsOtherDown(winOrLKey));
-            var disableLock = winLock.OnUp(e =>
-            {
-                // disable again, so when unlocked, *+Win+L would not trigger screen locking
-                DisableWinLock();
-            },  e => !e.KeyboardState.IsOtherDown(winOrLKey));
-            Application.Current.Dispatcher.BeginInvoke((Action)(() =>
-            {
-                Application.Current.DispatcherUnhandledException += (_, __) => EnableWinLock();
-                AppDomain.CurrentDomain.UnhandledException       += (_, __) => EnableWinLock();
-                //Application.Current.Exit                         += (_, __) => EnableWinLock();
-                Console.CancelKeyPress                           += (_, __) => EnableWinLock();
-                ConsoleExt.Exit                                  += EnableWinLock;
-            }));
-            
+			DisableWinLock();
+			var winLock = Key.Win + Key.L;
+			var winOrLKey = Key.Win | Key.L | Key.Ctrl | Key.Alt | Key.Shift;
+			var enableLock = winLock.OnDown(e =>
+			{
+				// only when LWin+L pressed, enable to lock
+				EnableWinLock();
+				// next the down event go to the system handler, it will trigger the win lock function
+			},
+				// *+win+L disable winlock
+				e => !e.KeyboardState.IsOtherDown(winOrLKey));
 
-        }
-    }
+			var disableLock = winLock.OnUp(e =>
+			{
+				// disable again, so when unlocked,
+				DisableWinLock();
+			},
+				// *+Win+L up would not trigger this handler
+				e => !e.KeyboardState.IsOtherDown(winOrLKey));
+
+			// when the plugin application exits, reenable normal handling
+			Application.Current.Dispatcher.BeginInvoke((Action)(() =>
+			{
+				Application.Current.DispatcherUnhandledException += (_, __) => EnableWinLock();
+				AppDomain.CurrentDomain.UnhandledException += (_, __) => EnableWinLock();
+				//Application.Current.Exit                         += (_, __) => EnableWinLock();
+				Console.CancelKeyPress += (_, __) => EnableWinLock();
+				ConsoleExt.Exit += EnableWinLock;
+			}));
+
+
+		}
+	}
 }
