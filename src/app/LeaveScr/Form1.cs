4321a2fca9683c2b3f2f79e9b1dfbe56aa3a42b3
metasong <metaseed@gmail.com>
2024-02-29 04:39:57 -0600

    ok

---
+++ src/app/LeaveScr/Form1.cs
@@ -0,0 +1,10 @@
+namespace LeaveScr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/app/LeaveScr/LeaveSrc/Form1.cs
@@ -0,0 +1,10 @@
+namespace LeaveSrc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
